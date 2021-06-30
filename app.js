f85d31b01454297266d5bb002c0047daa9d11743
AhmadMurad <>
2021-06-30 14:01:22 +0000

    added js

---
+++ app.js
@@ -0,0 +1,66 @@
+// a comment 
+/* 
+multi comment 
+*/
+/* syntax 
+
+Data types : 
+- numbers: 1 , 5 , -7
+
+- boolean : true/false
+
+- strings : "ahmad" , 'feygfrjj 134'
+
+variables :
+
+- to store anything in them 
+
+var name = "ahmad"
+
+var x = 10
+
+var sum = 0 + 10 => 10
+
+operators :
+
+arthmatic operators : 
+
++ , _ , * , /
+
+1+1
+7*9
+
+assigment operator :  
+
+= 
+
+*/ 
+
+
+var firstname = "Ahlam"
+
+console.log(firstname)
+
+console.log("hello world")
+
+
+var name = prompt("what is your name?")
+
+alert("welcome to Anime website" +  name)
+
+var favAnime = prompt("what is your fav Anime?")
+
+if(favAnime == "Hunter x Hunter"){
+document.write("Hunter x Hunter") 
+
+}
+
+else if (favAnime == "Naruto")
+{
+  document.write("<p>"+"great Anime choice")
+
+}
+else {
+  alert("sorry we don't have Anime")
+
+}
